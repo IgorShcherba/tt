b31475e2be6b8239bb17f656ffa471c43c55d575
Igor Shcherba <igor.shcherba@callstack.com>
2025-01-13 22:03:12 +0100

    finished chalenges from section 6

--- src/06-challenges/37-get-dynamic-path-params.problem.ts
+++ src/06-challenges/37-get-dynamic-path-params.problem.ts
@@ -1,10 +1,15 @@
+import { Split } from "ts-toolbelt/out/String/Split";
 import { Equal, Expect } from "../helpers/type-utils";
 
 type UserPath = "/users/:id";
 
 type UserOrganisationPath = "/users/:id/organisations/:organisationId";
 
-type ExtractPathParams = unknown;
+type ExtractPathParams<T extends string> = {
+  [K in Split<T, "/">[number] as K extends `:${infer P}` ? P : never]: string;
+};
+
+type Ex = ExtractPathParams<UserPath>;
 
 type tests = [
   Expect<Equal<ExtractPathParams<UserPath>, { id: string }>>,
